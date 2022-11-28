dfe61f55009e8e0b17a449406b247919009714e7
Tikpeme <tete.ikpeme@gmail.com>
2022-11-28 08:13:20 -0500

    Updating skills component

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import ReactTooltip from "react-tooltip";
+import { motion } from "framer-motion";
+
+import { AppWrap } from "../../wrapper";
+
+import { urlFor, client } from "../../client";
+
+import "./Skills.scss";
 
 import "./Skills.scss";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  const [skills, setSkills] = useState([]);
+  const [experiences, setExperiences] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query).then((data) => {
+      setExperiences(data);
+    });
+
+    client.fetch(skillsQuery).then((data) => {
+      setSkills(data);
+    });
+  }, []);
+
+  return (
+    <>
+      <h2 className="head-text">Skills & Experience </h2>
+      <div className="app__skill-container">
+        <motion.div className="app__skill-list">
+          {skills.map((skill) => (
+            <motion.div whileInView={{ opacity: [0, 1] }}></motion.div>
+          ))}
+        </motion.div>
+      </div>
+    </>
+  );
 };
 
 export default Skills;
